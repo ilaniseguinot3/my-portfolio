fedb583b0d57ff3018eded4c027c819f2ee41946
ilaniseguinot3 <i.seguinot@yahoo.com>
2025-12-29 17:22:33 -0500

    added header

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 export default function Page() {
   return (
     <div>
-      <Hero />
-      <Projects />
+      <Header />
+      <div id="hero"><Hero /></div>
+      <div id="projects"><Projects /></div>
       <Footer />
     </div>
   );
