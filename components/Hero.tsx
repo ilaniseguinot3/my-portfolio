977cc7105d76b07af62fcba0501ef1f8b4168d83
ilaniseguinot3 <i.seguinot@yahoo.com>
2025-12-29 13:16:01 -0500

    homepage edits: gradient background
    gradient background from pink to beige

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,18 +1,18 @@
-"use client";
-import { motion } from "framer-motion";
+  "use client";
+  import { motion } from "framer-motion";
 
-const Hero: React.FC = () => {
-  return (
-    <motion.div
-      className="h-screen flex flex-col justify-center items-center bg-gradient-to-r from-purple-500 to-pink-500 text-white"
-      initial={{ opacity: 0, y: -50 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 1 }}
-    >
-      <h1 className="text-5xl font-bold mb-4">Hi, I'm Ilani</h1>
-      <p className="text-xl">I build interactive, modern web experiences.</p>
-    </motion.div>
-  );
-};
+  const Hero: React.FC = () => {
+    return (
+      <motion.div
+        className="h-screen flex flex-col justify-center items-center bg-gradient-to-b from-[#F19C79] to-[#F6F4D2] text-[#A44A3F]"
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2 }}
+      >
+        <h1 className="text-xl font-bold mb-4">Hi, I'm Ilani</h1>
+        <p className="text-3xl">I am a full stack software developer.</p>
+      </motion.div>
+    );
+  };
 
-export default Hero;
+  export default Hero;
